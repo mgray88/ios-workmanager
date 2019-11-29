833b36276903f6604fb7beb18869c2b901d42a4b
Frank Gregor <phranck@cocoanaut.com>
2019-11-29 01:18:16 +0100

    cleans up code

--- Workmanager/Classes/WorkManager.swift
+++ Workmanager/Classes/WorkManager.swift
@@ -1,31 +1,31 @@
 import BackgroundTasks
 
 public class WorkManager {
-    
+
     // MARK: Properties
-    
+
     internal let scheduler = WorkScheduler()
     internal var scheduledTasks: Set<ScheduledTask> = []
-    
+
     // MARK: Initialization
-    
+
     public static let shared = WorkManager()
     private init() {}
-    
+
     // MARK: Registering
-    
+
     public func registerTask(withIdentifier identifier: String, onTrigger: @escaping (BGTask) -> ()) -> Bool {
         return BGTaskScheduler.shared.register(forTaskWithIdentifier: identifier, using: nil) { task in
             onTrigger(task)
         }
     }
-    
+
     // MARK: Scheduling
-    
+
     public func schedule(task: Task) throws {
         if let existingWorkPolicy = task.existingWorkPolicy,
             let _ = getScheduledTask(withIdentifier: task.identifier) {
-            
+
             switch existingWorkPolicy {
             case .keep:
                 return
@@ -33,52 +33,49 @@ public class WorkManager {
                 cancelTask(withIdentifier: task.identifier)
             }
         }
-        
+
         try scheduler.scheduleTask(task) { request in
             scheduledTasks.insert(ScheduledTask(task: task, request: request))
         }
     }
-    
+
     // MARK: Callbacks
-    
-    public func taskDidFinish(_ task: BGTask, success: Bool) throws {
+
+    public func finish(task: BGTask, success: Bool) throws {
         task.setTaskCompleted(success: success)
-        
+
         guard let scheduledTask = getScheduledTask(forCompletedTask: task) else {
             return
         }
-        
+
         guard success else {
             try handleError(withScheduledTask: scheduledTask)
             return
         }
-        
+
         try handleSuccess(withScheduledTask: scheduledTask)
     }
-    
+
     // MARK: Success handlers
-    
+
     private func handleSuccess(withScheduledTask scheduledTask: ScheduledTask) throws {
         if scheduledTask.task.isPeriodic {
-            try handlePeriodicTaskSuccess(withScheduledTask: scheduledTask)
+            let task = scheduledTask.task
+            try schedule(task: task)
+
         } else {
             removeScheduledTask(scheduledTask)
         }
     }
-    
-    private func handlePeriodicTaskSuccess(withScheduledTask scheduledTask: ScheduledTask) throws {
-        let previousTask = scheduledTask.task
-        try schedule(task: previousTask)
-    }
-    
+
     // MARK: Error handlers
-    
+
     private func handleError(withScheduledTask scheduledTask: ScheduledTask) throws {
         var task = scheduledTask.task
         guard let backoffPolicy = task.backoffPolicy else { return }
 
         var delay = 0.0
-        
+
         switch backoffPolicy {
             case .linear:
                 delay = task.initialDelay + task.backoffPolicyDelay
@@ -89,7 +86,7 @@ public class WorkManager {
                     delay = pow(task.initialDelay, 2)
             }
         }
-        
+
         task.initialDelay = delay
         try schedule(task: task)
     }
